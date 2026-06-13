54f95e2ed6898ba603a0f3ad861bd84dc09dc8c2
Manmohan Kushwaha <mkush575@gmail.com>
2026-06-13 23:34:57 +0530

    hash otps for security

--- src/main/java/com/devms/otpservice/service/OtpSerVice.java
+++ src/main/java/com/devms/otpservice/service/OtpSerVice.java
@@ -1,25 +1,57 @@
 package com.devms.otpservice.service;
 
+import com.devms.otpservice.Components.OtpHasher;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
-//@NoArgsConstructor
-public class OtpSerVice {
+public class OtpSerVice
+{
+    private final SecureRandom secureRandom=new SecureRandom();
+    private final OtpHasher otpHasher;
     EmailService emailService;
     @Value("${approvers}")
     List<String> adminEmail;
 
-    public String generateOtp() {
+    Map<String, String> hasedOtps=new HashMap<>();
+
+    public String sendOtp()
+    {
         for(String email: adminEmail)
         {
-            emailService.sendEmail(email,"From Spring boot application", "Test text");
+            String otp=generateOtp();
+            try
+            {
+                hasedOtps.put(email,otpHasher.hashOtp(email,otp));
+            }
+            catch (GeneralSecurityException e)
+            {
+                throw new IllegalStateException("Couldn't perform otp hashing: ",e);
+            }
+
+//            otps.put(email,Integer.valueOf(otp));
+            emailService.sendEmail(email,"Otp service", "Your otp to approve vault data update is: "+otp);
         }
+        hasedOtps.forEach(
+                (key,value) ->
+                {
+                    System.out.println("Key: "+key+" Value: "+value);
+                }
+        );
         return "otp sent successfully";
+    }
+
+    public String generateOtp()
+    {
+        return String.format("%06d",secureRandom.nextInt(1_000_000));
     }
 }
